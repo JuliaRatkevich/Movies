06cf0a100bdb246224f135f617823f6385692458
Julia <juliakuntsevich@gmail.com>
2022-11-08 12:44:37 +0100

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Yuliya Ratkevcih on 08.11.22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
